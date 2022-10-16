87eb31e2050f62b578c261b73bfcce4f68cc89ce
ykuang321 <112137899+ykuang321@users.noreply.github.com>
2022-10-16 07:22:06 -0500

    add timeblock in html, add function to script, and modified css

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,39 @@
 var currentDay = document.getElementById('currentDay');
 var timeBlockHour = document.getElementsByClassName('hour');
-var saveEvent = document.getElementsByClassName('saveBtn');
-var displayEventColor = document.getElementsByClassName('description');
-
 var currentHour = moment().format('H');
+var displayEventColor = document.getElementsByClassName('description');
 
 //display current day/time in header
 setInterval(() => {
   currentDay.textContent = moment().format('LLLL');
 }, 1000);
 
-//for loop to compare current time with standard business hours and then display color based on result
+//for loop to compare the current hour with standard business hours and then display color based on result
 for (var i=0; i<timeBlockHour.length; i++){  
 
-  if(currentHour > timeBlockHour[i].dataset['number']){
+  //convert string to int for comparison
+  var compCurrentHour = parseInt(currentHour);
+  var compTableHour = parseInt(timeBlockHour[i].dataset['number']);
+
+  if(compCurrentHour > compTableHour){
     displayEventColor[i].classList.add("past");
   }
 
-  else if(currentHour === timeBlockHour[i].dataset['number']){
+  else if(compCurrentHour === compTableHour){
     displayEventColor[i].classList.add("present");
   }
 
-  else if(currentHour < timeBlockHour[i].dataset['number']){
+  else if(compCurrentHour < compTableHour){
     displayEventColor[i].classList.add("future");
   }
 };
 
+//add event listener
+$(".saveBtn").on("click", function(){
 
+var textInput = $(this).find(".description");
+console.log(textInput);
+var test = textInput.val();
+console.log(test);
 
-
+});
